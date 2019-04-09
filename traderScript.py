20e189afb2eee62369230595dc8f7c52c2d70af9
jjjacobsen <jonah.jacobsen@colorado.edu>
2019-04-09 14:14:45 -0600

    added some blockchain symbols to see how they are doing

--- traderScript.py
+++ traderScript.py
@@ -49,24 +49,24 @@ def get_data(symbols):
 def check_patterns(data):
     bullish = []
     bearish = []
-    
+
     # at beginning define lengths and such to use for calculations later ---
-        
+
     # get percent change over past 14 days
     change = (data.iloc[99, 3] - data.iloc[85, 3]) / data.iloc[99, 3]
-    
+
     # get median of candle lengths to define small and large candles
     sizes = []
     for _, row in data.iterrows():
         sizes.append(abs(row['open']-row['close']))
     median = np.percentile(sizes, 50)
-    
+
     # these are the most recent candles, the important ones we will analyze
     one = data.iloc[96]
     two = data.iloc[97]
     three = data.iloc[98]
     four = data.iloc[99]
-    
+
     # this grabs more data about the candlesticks
     def candle_detail(c):
         color, size, uw, dw = None, None, None, None
@@ -89,13 +89,13 @@ def check_patterns(data):
             uw = c['high'] - c['close']
             dw = c['open'] - c['low']
         return l, color, uw, dw, size
-    
+
     # run function on important candles
     len1, color1, uwick1, dwick1, size1 = candle_detail(one)
     len2, color2, uwick2, dwick2, size2 = candle_detail(two)
     len3, color3, uwick3, dwick3, size3 = candle_detail(three)
     len4, color4, uwick4, dwick4, size4 = candle_detail(four)
-    
+
     # used to determine if there was up or down trend before checking for patterns
     def trend_find(day):
         if (data.iloc[day, 3] - data.iloc[day-4, 3]) > 0:
@@ -103,10 +103,10 @@ def check_patterns(data):
         else:
             return 'down'
     trend = None
-    
+
     # Bullish Single Candle Patterns ---
     trend = trend_find(97)
-    
+
     # Bullish Hammer
     if trend == 'down':
         if dwick3 >= (2 * len3) and uwick3 <= (0.15 * len3):
@@ -114,7 +114,7 @@ def check_patterns(data):
                 bullish.append('Green Bullish Hammer')
             else:
                 bullish.append('Red Bullish Hammer')
-            
+
     # Inverted Hammer
     if trend == 'down':
         if uwick3 >= (2 * len3) and dwick3 <= (0.15 * len3):
@@ -122,31 +122,31 @@ def check_patterns(data):
                 bullish.append('Green Inverted Hammer')
             else:
                 bullish.append('Red Inverted Hammer')
-            
+
     # Belt Hold
     if trend == 'down':
         if color3 == 'green':
             if size3 == 'long':
                 if dwick3 <= (0.05 * len3) and uwick3 <= (0.05 * len3):
                     bullish.append('Belt Hold')
-                
+
     # Bullish Two Candle Patterns ---
     trend = trend_find(96)
-    
+
     # Harami
     if trend == 'down':
         if color2 == 'red' and color3 == 'green':
             if size2 == 'long' and size3 == 'short':
                 if three['high'] < two['open'] and three['low'] > two['close']:
                     bullish.append('Harami')
-                
+
     # Harami Cross
     if trend == 'down':
         if color2 == 'red' and color3 == 'doji':
             if size2 == 'long':
                 if three['high'] < two['open'] and three['low'] > two['close']:
                     bullish.append('Harami Cross')
-                
+
     # Homing Pigeon
     if trend == 'down':
         if color2 == 'red' and color3 == 'red':
@@ -154,14 +154,14 @@ def check_patterns(data):
                 if three['high'] < two['high'] and three['low'] > two['low']:
                     if three['open'] < two['open'] and three['close'] > two['close']:
                         bullish.append('Homing Pigeon')
-                
+
     # Matching Low
     if trend == 'down':
         if color2 == 'red':
             if size2 == 'red':
                 if three['low'] == two['low']:
                     bullish.append('Matching Low')
-                
+
     # Piercing Line
     if trend == 'down':
         if color2 == 'red' and color3 == 'green':
@@ -170,7 +170,7 @@ def check_patterns(data):
                     if uwick3 <= (0.15 * len3):
                         if three['close'] > ((two['open'] + two['close'])/2) and three['close'] < two['open']:
                             bullish.append('Piercing Line')
-                    
+
     # Meeting Line
     if trend == 'down':
         if color2 == 'red' and color3 == 'green':
@@ -179,32 +179,32 @@ def check_patterns(data):
                     if uwick3 <= (0.15 * len3):
                         if three['close'] == two['close']:
                             bullish.append('Piercing Line')
-                        
+
     # One Green Soldier
     if trend == 'down':
         if color2 == 'red' and color3 == 'green':
             if size2 == 'long' and size3 == 'long':
                 if three['open'] > two['close'] and three['close'] > two['open']:
                     bullish.append('One Green Soldier')
-                
+
     # Engulfing
     if trend == 'down':
         if color2 == 'red' and color3 == 'green':
             if size2 == 'long' and size3 == 'long':
                 if three['high'] > two['high'] and three['low'] < two['low']:
                     bullish.append('Engulfing')
-                
+
     # Kicking/Kicker
     if trend == 'down':
         if color2 == 'red' and color3 == 'green':
             if size2 == 'long':
                 if three['open'] > two['open']:
                     if uwick3 <= (0.15 * len3):
-                        bullish.append('Kicking/Kicker')     
-                
+                        bullish.append('Kicking/Kicker')
+
     # Bullish Three Candle Patterns ---
     trend = trend_find(95)
-    
+
     # Morning Star
     if trend == 'down':
         if color1 == 'red' and color2 == 'green' and color3 == 'green':
@@ -212,7 +212,7 @@ def check_patterns(data):
                 if two['open'] < one['close']:
                     if three['close'] < one['open'] and three['close'] > one['close']:
                         bullish.append('Morning Star')
-                    
+
     # Doji Morning Star
     if trend == 'down':
         if color1 == 'red' and color2 == 'doji' and color3 == 'green':
@@ -220,7 +220,7 @@ def check_patterns(data):
                 if two['open'] < one['close']:
                     if three['close'] < one['open'] and three['close'] > one['close']:
                         bullish.append('Doji Morning Star')
-                    
+
     # Abandoned Baby
     if trend == 'down':
         if color1 == 'red' and color2 == 'doji' and color3 == 'green':
@@ -229,7 +229,7 @@ def check_patterns(data):
                     if three['low'] > two['high']:
                         if three['close'] < one['open'] and three['close'] > one['close']:
                             bullish.append('Abandoned Baby')
-                    
+
     # Two Rabbits
     if trend == 'down':
         if color1 == 'red' and color2 == 'green' and color3 == 'green':
@@ -237,7 +237,7 @@ def check_patterns(data):
                 if two['close'] < one['close']:
                     if three['open'] > two['open'] and three['close'] > two['close']:
                         bullish.append('Two Rabbits')
-                    
+
     # Downside Gap Two Rabbits
     if trend == 'down':
         if color1 == 'red' and color2 == 'green' and color3 == 'green':
@@ -245,7 +245,7 @@ def check_patterns(data):
                 if two['close'] < one['close']:
                     if three['open'] < two['open'] and three['close'] > two['close']:
                         bullish.append('Downside Gap Two Rabbits')
-                        
+
     # Deliberation Block
     if trend == 'down':
         if color1 == 'red' and color2 == 'red' and color3 == 'red':
@@ -253,14 +253,14 @@ def check_patterns(data):
                 if two['open'] > one['close'] and two['open'] < one['open']:
                     if three['open'] < two['close']:
                         bullish.append('Deliberation Block')
-                    
+
     # Decent Block
     if trend == 'down':
         if color1 == 'red' and color2 == 'red' and color3 == 'red':
             if size1 == 'long' and size2 == 'long' and size3 == 'long':
                 if len1 > len2 and len2 > len3:
                     bullish.append('Decent Block')
-                
+
     # Three Green Soldiers
     if trend == 'down':
         if color1 == 'green' and color2 == 'green' and color3 == 'green':
@@ -269,25 +269,25 @@ def check_patterns(data):
                     if two['open'] > one['low'] and two['open'] < one['high']:
                         if three['open'] > two['low'] and three['open'] < two['high']:
                             bullish.append('Three Green Soldiers')
-                            
+
     # Check for bullish confirmation ---
     if bullish:
         if color4 == 'green':
             bullish.append('Next Day Confirmation')
-            
+
     # return early if we know the stock is bullish
     if bullish:
         return bullish, bearish, change
-            
+
     # Bearish Single Candle Patterns ---
     trend = trend_find(97)
-    
+
     # Hanging Man
     if trend == 'up':
         if size3 == 'small':
             if dwick3 >= (2 * len3) and uwick3 < (0.15 * len3):
                 bearish.append('Hanging Man')
-    
+
     # Bearish Belt Hold
     if trend == 'up':
         if color3 == 'red':
@@ -295,10 +295,10 @@ def check_patterns(data):
                 if uwick3 <= (0.2 * len3):
                     if three['open'] > two['high']:
                         bearish.append('Bearish Belt Hold')
-                        
+
     # Bearish Two Candle Patterns ---
     trend = trend_find(96)
-    
+
     # Shooting Star
     if trend == 'up':
         if color2 == 'green':
@@ -308,27 +308,27 @@ def check_patterns(data):
                         bearish.append('Green Shooting Star')
                     else:
                         bearish.append('Red Shooting Star')
-                        
+
     # Bearish Harami
     if trend == 'up':
         if color2 == 'green' and color3 == 'red':
             if size3 == 'small':
                 if abs(three['open'] - three['close']) < abs(two['open'] - two['close']):
                     bearish.append('Bearish Harami')
-                    
+
     # Bearish Harami Cross:
     if trend == 'up':
         if color2 == 'green' and color3 == 'doji':
             if three['high'] < two['high'] and three['low'] > two['low']:
                 bearish.append('Bearish Harami Cross')
-                
+
     # Bearish Doji Star
     if trend == 'up':
         if color2 == 'green' and color3 == 'doji':
             if uwick2 <= (0.2 * len2):
                 if three['open'] > two['close']:
                     bearish.append('Bearish Doji Star')
-                    
+
     # Matching High
     if trend == 'up':
         if color2 == 'green' and color3 == 'green':
@@ -336,21 +336,21 @@ def check_patterns(data):
                 if three['open'] > two['open']:
                     if three['close'] == two['close']:
                         bearish.append('Matching High')
-                        
+
     # Descending Hawk
     if trend == 'up':
         if color2 == 'green' and color3 == 'green':
             if size3 == 'small':
                 if three['open'] > two['open'] and three['close'] < two['close']:
                     bearish.append('Descending Hawk')
-                    
+
     # Bearish Meeting Line
     if trend == 'up':
         if color2 == 'green' and color3 == 'red':
             if uwick2 <= (0.2 * len2):
                 if three['close'] == two['close']:
                     bearish.append('Bearish Meeting Line')
-                    
+
     # Cloud Cover
     if trend == 'up':
         if color2 == 'green' and color3 == 'red':
@@ -358,7 +358,7 @@ def check_patterns(data):
                 if three['open'] > two['close']:
                     if three['close'] < ((two['open'] + two['close'])/2):
                         bearish.append('Cloud Cover')
-                        
+
     # One Black Crow
     if trend == 'up':
         if color2 == 'green' and color3 == 'red':
@@ -366,7 +366,7 @@ def check_patterns(data):
                 if three['open'] > two['open'] and three['open'] < two['high']:
                     if three['close'] < two['open']:
                         bearish.append('One Black Crow')
-                        
+
     # Bearish Kicking
     if trend == 'up':
         if color2 == 'green' and color3 == 'red':
@@ -374,17 +374,17 @@ def check_patterns(data):
                 if three['open'] < two['open']:
                     if dwick3 <= (0.2 * len3):
                         bearish.append('Bearish Kicking')
-                        
+
     # Bearish Engulfing
     if trend == 'up':
         if color2 == 'green' and color3 == 'red':
             if size3 == 'long':
                 if three['open'] > two['close'] and three['close'] < two['open']:
                     bearish.append('Bearish Engulfing')
-                    
+
     # Bearish Three Candle Patterns ---
     trend = trend_find(95)
-    
+
     # Advance Block
     if trend == 'up':
         if color1 == 'green' and color2 == 'green' and color3 == 'green':
@@ -392,7 +392,7 @@ def check_patterns(data):
                 if three['open'] > two['open'] and three['open'] < two['close'] and three['close'] > two['close']:
                     if uwick2 >= (0.75 * len2) and uwick3 >= (0.75 * len3):
                         bearish.append('Advance Block')
-                        
+
     # Bearish Deliberation Block
     if trend == 'up':
         if color1 == 'green' and color2 == 'green' and color3 == 'green':
@@ -400,7 +400,7 @@ def check_patterns(data):
                 if two['open'] > one['open'] and two['open'] < one['close'] and two['close'] > one['close']:
                     if three['open'] > two['close']:
                         bearish.append('Bearish Deliberation Block')
-                
+
     # Evening Star
     if trend == 'up':
         if color1 == 'green' and color3 == 'red':
@@ -410,7 +410,7 @@ def check_patterns(data):
                         if three['close'] > one['open'] and three['close'] < one['close']:
                             if dwick3 <= (0.2 * len3):
                                 bearish.append('Evening Star')
-                                
+
     # Evening Doji
     if trend == 'up':
         if color1 == 'green' and color2 == 'doji' and color3 == 'red':
@@ -420,7 +420,7 @@ def check_patterns(data):
                         if dwick3 <= (0.2 * len3):
                             if three['open'] < two['low']:
                                 bearish.append('Evening Star')
-                                
+
     # Bearish Abandoned Baby
     if trend == 'up':
         if color1 == 'green' and color3 == 'red':
@@ -428,7 +428,7 @@ def check_patterns(data):
                 if two['low'] > one['high']:
                     if three['high'] < two['low']:
                         bearish.append('Bearish Abandoned Baby')
-                        
+
     # Two Crows
     if trend == 'up':
         if color1 == 'green' and color2 == 'red' and color3 == 'red':
@@ -438,7 +438,7 @@ def check_patterns(data):
                         if three['open'] > two['close'] and three['open'] < two['open']:
                             if three['close'] > one['open'] and three['close'] < one['close']:
                                 bearish.append('Two Crows')
-                                
+
     # Three Black Crows
     if trend == 'up':
         if color1 == 'red' and color2 == 'red' and color3 == 'red':
@@ -447,12 +447,12 @@ def check_patterns(data):
                     if two['open'] > one['close'] and two['open'] < one['open'] and two['close'] < one['close']:
                         if three['open'] > two['close'] and three['open'] < two['open']:
                             bearish.append('Three Black Crows')
-                            
+
     # Check for bearish confirmation ---
     if bearish:
         if color4 == 'red':
             bearish.append('Next Day Confirmation')
-    
+
     return bullish, bearish, change
 
 def bullish_patterns(r):
@@ -525,7 +525,8 @@ symbols = ['CGC', 'TWLO', 'AMD', 'CRON', 'ACB', 'ECL', 'SQ', 'MU', 'SPOT', 'DBX'
             'QCOM', 'PG', 'JPM', 'GM', 'ORCL', 'IIPR', 'NEE', 'BABA', 'ATVI', 'SHOP', 'SNE', 'EA',
             'TMUS', 'OKTA', 'CTL', 'BB', 'WB', 'DOCU', 'EBAY', 'TEAM', 'HEAR', 'MDB', 'GRUB', 'PVTL',
             'WDAY', 'LOGI', 'VMW', 'RHT', 'DELL', 'GDDY', 'NTGR', 'CARB', 'AKAM', 'NKE', 'TCEHY',
-            'PFE', 'COST', 'CRSP', 'LUV', 'BA', 'CVS', 'TGT', 'ABBV', 'QQQ', 'MELI', 'GH']
+            'PFE', 'COST', 'CRSP', 'LUV', 'BA', 'CVS', 'TGT', 'ABBV', 'QQQ', 'MELI', 'GH', 'RIOT',
+            'BTC', 'BCH', 'LTC', 'ETH', 'DOGE']
 
 get_data(symbols)
 
